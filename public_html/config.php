0347c9e1ee6d155a1f63431f46674f445e9f3a84
ClickAhorroCO <clickahorroco@gmail.com>
2026-01-05 18:18:45 -0500

    aprende

---
+++ public_html/config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost:3306';
+$CFG->dbname    = 'moodle_db';
+$CFG->dbuser    = 'user';
+$CFG->dbpass    = 'xxxxxxxx';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'https://aprendeconmeintegral.com.co';
+$CFG->dataroot  = '/home/aprende3/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
